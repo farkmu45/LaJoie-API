9119657d5e443c78613b097f013db13ec95fb401
farkmu45 <farukmicros@outlook.com>
2021-11-24 10:34:06 +0700

    Add authentication guard in walls route

--- walls.php
+++ walls.php
@@ -3,6 +3,7 @@ require_once('./modules/Auth.php');
 require_once('./models/Question.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    Auth::guard();
     Question::getAll();
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $userId =  Auth::guard();
