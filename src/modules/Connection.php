fb9b973bcbd2dd34687ac6b799510c68aa7db277
farkmu45 <farukmicros@outlook.com>
2021-11-24 16:45:27 +0700

    Use .env to mask db information

--- src/modules/Connection.php
+++ src/modules/Connection.php
@@ -2,21 +2,30 @@
 
 namespace App\modules;
 
+use Dotenv\Dotenv;
 use PDO;
 use PDOException;
+
 
 class Connection
 {
     public $db;
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $databaseName = 'lajoie';
+
 
     public function __construct()
     {
+        if (file_exists(__DIR__ . '/.env')) {
+            $dotenv = Dotenv::createImmutable(__DIR__);
+            $dotenv->load();
+        }
+
+        $host = $_ENV['DB_HOST'];
+        $username = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $databaseName = $_ENV['DB_NAME'];
+        
         try {
-            $this->db = new PDO("mysql:host=$this->host;dbname=$this->databaseName", $this->username, $this->password);
+            $this->db = new PDO("mysql:host=$host;dbname=$databaseName", $username, $password);
         } catch (PDOException $e) {
             echo $e->getMessage();
             die();
