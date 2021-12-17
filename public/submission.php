8a1e9ef48a757c9eb3cdb57eceea69f3420a56c0
farkmu45 <farukmicros@outlook.com>
2021-12-17 20:36:42 +0700

    Use uploadcare to upload file

--- public/submission.php
+++ public/submission.php
@@ -5,21 +5,20 @@ require '../vendor/autoload.php';
 use LaJoie\modules\Auth;
 use LaJoie\modules\Response;
 use LaJoie\models\Submission;
-use LaJoie\models\User;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = Auth::guard();
+    $body = json_decode(file_get_contents('php://input'), true);
 
-    $experience = $_POST['experience'];
-    $docName = $_FILES['document']['name'];
-    $docLocation = $_FILES['document']['tmp_name'];
-    $docSize = $_FILES['document']['size'];
+    $experience = $body['experience'];
+    $document = $body['document'];
 
-    $fileExt = strtolower(pathinfo($docName, PATHINFO_EXTENSION));
-    $uploadPath = 'images/documents/';
-    $document = uniqid() .'.'. $fileExt;
-    Submission::create($user, $experience, $uploadPath.$document);
-    move_uploaded_file($docLocation, $uploadPath . $document);
+
+    // $fileExt = strtolower(pathinfo($docName, PATHINFO_EXTENSION));
+    // $uploadPath = 'images/documents/';
+    // $document = uniqid() .'.'. $fileExt;
+    Submission::create($user, $experience, $document);
+    // move_uploaded_file($docLocation, $uploadPath . $document);
 
 } else {
     new Response(["message" => "Not found"], Response::$NOT_FOUND);
